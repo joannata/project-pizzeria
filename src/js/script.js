952590ef0ef7fa1fb8067de5b7ff403fcf1ca222
Joanna Tarnowska <joanna.tarnowska.91@gmail.com>
2022-03-29 18:37:43 +0200

    Remove active class in data.js file

--- src/js/script.js
+++ src/js/script.js
@@ -106,9 +106,11 @@
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
-        //[TO UNCOVER!!] const activeProduct = document.querySelector(select.all.menuProductsActive);
-        /* if there is active product and it's not thisProduct.element, remove class active from it */
-        //[TO DO!!] if(activeProduct != thisProduct.element ...) *
+        /* [TO UNCOVER!!] const activeProduct = document.querySelector(select.all.menuProductsActive);
+        /* [TO DO!!] if there is active product and it's not thisProduct.element, remove class active from it */
+        /* if(activeProduct !== thisProduct.element){
+          activeProduct.classList.remove('active');
+        }
 
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
